a1b27e706b225a5b87d201a2c39500f1a98c037d
Алмас Тажиев <almas200224@gmail.com>
2025-11-19 00:45:11 +0500

    add snippet in layouts

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -20,4 +20,54 @@ const Layouts = () => {
   );
 };
 
+/*
+{
+	"Export default component": {
+		"prefix": "exr",
+		"body": [
+			"export { default } from './${TM_FILENAME}'"
+		],
+		"description": "Log output to console",
+	},
+	"React FC with children": {
+		"prefix": "fc",
+		"body": [
+			"import { FC } from 'react';",
+			"",
+			"interface Props {",
+			"  className?: string;",
+			"  children: React.ReactNode;",
+			"}",
+			"",
+			"const ${TM_FILENAME_BASE/(.*)/${1:/pascalcase}/}: FC<Props> = ({ children, className }) => {",
+			"  return (",
+			"    <>",
+			"      {children}",
+			"    </>",
+			"  );",
+			"};",
+			"",
+			"export default ${TM_FILENAME_BASE/(.*)/${1:/pascalcase}/};"
+		],
+		"description": "React Functional Component with children (TypeScript)"
+	},
+	"React default ts function": {
+		"prefix": "rfc",
+		"body": [
+			"const ${TM_FILENAME_BASE/(.*)/${1:/pascalcase}/} = () => {",
+			"  return (",
+			"    <>",
+			"      <h2>${TM_FILENAME_BASE/(.*)/${1:/pascalcase}/}</h2>",
+			"      <h2>${TM_FILENAME_BASE/(.*)/${1:/pascalcase}/}</h2>",
+			"    </>",
+			"  );",
+			"};",
+			"",
+			"export default ${TM_FILENAME_BASE/(.*)/${1:/pascalcase}/};"
+		],
+		"description": "React Functional Component with children (TypeScript)"
+	}
+}
+*/
+
 export default Layouts;
